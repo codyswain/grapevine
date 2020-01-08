cab357bab815a81896bbbe0283e41d90ae402d1a
Anthony Humay <anthony.humay@gmail.com>
2020-01-08 14:38:04 -0800

    MVC folders

---
+++ Grapevine/Model/Post.swift
@@ -0,0 +1,15 @@
+//
+//  Post.swift
+//  Grapevine
+//
+//  Created by Anthony Humay on 1/8/20.
+//  Copyright © 2020 Grapevine. All rights reserved.
+//
+
+import Foundation
+
+struct Post {
+    let content: String
+    let upvotes: Int
+    let downvotes: Int
+}
