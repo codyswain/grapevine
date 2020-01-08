6c81683cdbfd32b126e1f8f0e229be10e0d9ef51
Anthony Humay <anthony.humay@gmail.com>
2020-01-08 15:16:37 -0800

    Basic table

---
+++ Grapevine/Constants.swift
@@ -0,0 +1,28 @@
+//
+//  Constants.swift
+//  Grapevine
+//
+//  Created by Anthony Humay on 1/8/20.
+//  Copyright © 2020 Grapevine. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let appName = "Grapevine"
+    static let cellIdentifier = "ReusableCell"
+
+//    struct BrandColors {
+//        static let purple = "BrandPurple"
+//        static let lightPurple = "BrandLightPurple"
+//        static let blue = "BrandBlue"
+//        static let lighBlue = "BrandLightBlue"
+//    }
+//
+//    struct FStore {
+//        static let collectionName = "messages"
+//        static let senderField = "sender"
+//        static let bodyField = "body"
+//        static let dateField = "date"
+//    }
+}
--- Grapevine/Controller/ViewController.swift
+++ Grapevine/Controller/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts: [Post] = [
+        Post(content:"Hi Cody", upvotes:5, downvotes:0),
+        Post(content:"Jason is a tank", upvotes:3, downvotes:0)
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = posts[indexPath.row].content
+
+        return cell
+    }
+}
