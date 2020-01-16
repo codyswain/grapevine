13f53be23c5ef400214e6bf68a86dffd4c0c9d1e
Anthony Humay <anthony.humay@gmail.com>
2020-01-15 17:02:58 -0800

    Major UI edits

--- Grapevine/Constants.swift
+++ Grapevine/Constants.swift
@@ -13,6 +13,7 @@ struct Constants {
     static let appName = "Grapevine"
     static let cellIdentifier = "ReusableCell"
     static let cellNibName = "PostTableViewCell"
+    static let numberOfCharactersPerPost = 280
     static let numberOfPostsPerBatch = 20
     struct Firestore {
         static let collectionName = "posts"
