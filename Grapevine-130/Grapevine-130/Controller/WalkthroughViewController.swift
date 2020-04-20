a248c8369d2c15dc46a45f30911c85ff929ac6bd
Cody Swain <swain.cody@gmail.com>
2020-04-19 21:19:52 -0700

    Fixed walkthrough image size on older iphone models

--- Grapevine-130/Grapevine-130/Controller/WalkthroughViewController.swift
+++ Grapevine-130/Grapevine-130/Controller/WalkthroughViewController.swift
@@ -49,8 +49,8 @@ class WalkthroughViewController: UICollectionViewController, UICollectionViewDel
             cell.continueImageView.image = UIImage(named: "")
         case 1:
             cell.pageImageView.image = UIImage(named: "Ghost")
-            let attrText = NSMutableAttributedString(string: "an anonymous,", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 24),  NSAttributedString.Key.foregroundColor: UIColor.white])
-            attrText.append(NSAttributedString(string: "\n→", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 40), NSAttributedString.Key.foregroundColor: UIColor.white]))
+            let attrText = NSMutableAttributedString(string: "an anonymous...", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 24),  NSAttributedString.Key.foregroundColor: UIColor.white])
+//            attrText.append(NSAttributedString(string: "\n→", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 26), NSAttributedString.Key.foregroundColor: UIColor.white]))
             cell.descriptionTextView.attributedText = attrText
             cell.descriptionTextView.textAlignment = .center
             cell.descriptionTextView.translatesAutoresizingMaskIntoConstraints = false
@@ -60,8 +60,8 @@ class WalkthroughViewController: UICollectionViewController, UICollectionViewDel
             cell.continueImageView.image = UIImage(named: "")
         case 2:
             cell.pageImageView.image = UIImage(named: "Map")
-            let attrText = NSMutableAttributedString(string: "location based,", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 24),  NSAttributedString.Key.foregroundColor: UIColor.white])
-            attrText.append(NSAttributedString(string: "\n→", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 40), NSAttributedString.Key.foregroundColor: UIColor.white]))
+            let attrText = NSMutableAttributedString(string: "location based...", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 24),  NSAttributedString.Key.foregroundColor: UIColor.white])
+//            attrText.append(NSAttributedString(string: "\n→", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 26), NSAttributedString.Key.foregroundColor: UIColor.white]))
             cell.descriptionTextView.attributedText = attrText
             cell.descriptionTextView.textAlignment = .center
             cell.descriptionTextView.translatesAutoresizingMaskIntoConstraints = false
@@ -71,7 +71,7 @@ class WalkthroughViewController: UICollectionViewController, UICollectionViewDel
             cell.continueImageView.image = UIImage(named: "")
         case 3:
             cell.pageImageView.image = UIImage(named: "Party")
-            cell.descriptionTextView.attributedText = NSMutableAttributedString(string: "social app!", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 24),  NSAttributedString.Key.foregroundColor: UIColor.white])
+            cell.descriptionTextView.attributedText = NSMutableAttributedString(string: "social app!", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 26),  NSAttributedString.Key.foregroundColor: UIColor.white])
             cell.descriptionTextView.textAlignment = .center
             cell.descriptionTextView.translatesAutoresizingMaskIntoConstraints = false
             cell.descriptionTextView.isEditable = false
